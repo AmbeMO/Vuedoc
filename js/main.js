8d483ff21ff061d83e412a28f063cb510eca3e3b
AmbeMO <milamo@mail.ua>
2021-02-04 23:23:14 +0200

    end 7 computed property

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: 'Calvin Klein',
         product: 'Underwear',
         description: 'White underwear for women',
-        image: "./img/white.png",
-        inStock:true,
+        selectedVariant:0,//based on index
         altImage: 'White Underwear',
         underWearLink: 'https://www.calvinklein.us/en',
         inventory: 5,
@@ -18,12 +18,14 @@ var app = new Vue({
             {
                 variantId:228,
                 variantColor:'#f7f7f7',
-                variantImage: './img/white.png'
+                variantImage: './img/white.png',
+                variantQuantity: 5
             },
             {
                 variantId:322,
                 variantColor:"#e2b6a9",
-                variantImage: './img/cream.png'
+                variantImage: './img/cream.png',
+                variantQuantity: 0
             }
         ],
         sizes:[
@@ -35,11 +37,29 @@ var app = new Vue({
         addToCart: function () {// == addToCart(){
             this.cart +=1
         },
-        updateProduct:function (variantImage) {// == updateProduct(variantImage){
-            this.image = variantImage
+        updateProduct:function (index) {// == updateProduct(variantImage){
+            this.selectedVariant = index
+            console.log(index)
         },
         decreaseFromCart(){// == decreaseFromCart:function(){
             this.cart -=1 && this.cart>0
+        }
+    },
+    computed: {
+        title(){
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale(){
+           if( this.onSale){
+               return this.brand + ' ' + this.product + 'are on sale'
+           }
+           return  this.brand + ' ' + this.product + 'aren`t on sale'
         }
     }
 })
