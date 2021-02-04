0aab67b2d9f16117e03a6c7e44347952929fafd4
AmbeMO <milamo@mail.ua>
2021-02-04 12:39:14 +0200

    end 3(conditional renderinf(v-if)) end 4 ( List rendering( v- for ))

--- js/main.js
+++ js/main.js
@@ -6,7 +6,26 @@ var app = new Vue({
         image: "./img/white.jpg",
         altImage: 'White Underwear',
         underWearLink: 'https://www.calvinklein.us/en',
-        inStock: true
+        inventory: 5,
+        onSale: true,
+        storages:[
+            '80% cotton',
+            '20% polyester'
+
+        ],
+        variants:[
+            {
+                variantId:228,
+                variantColor:"white"
+            },
+            {
+                variantId:322,
+                variantColor:"cream"
+            }
+        ],
+        sizes:[
+            36,38,40,42
+        ]
 
     }
 })
