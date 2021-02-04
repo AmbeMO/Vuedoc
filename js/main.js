f2fcf567aa13bdf6df416a932cd893fcceaadc0b
AmbeMO <milamo@mail.ua>
2021-02-04 11:13:19 +0200

    end 2

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Underwear',
-        description: 'White underwear',
-        
+        description: 'White underwear for women',
+        image: "./img/white.jpg",
+        altImage: 'White Underwear',
+        underWearLink: 'https://www.calvinklein.us/en',
+        inStock: true
 
     }
 })
